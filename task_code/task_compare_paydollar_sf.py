7ffaec6d00b72f33de3e5f56728cf0f43512853f
Muhammad Faez Mohammad Nizam <mfmohammad@unicef.org>
2024-05-15 14:06:54 +0800

    Update file

--- task_code/task_compare_paydollar_sf.py
+++ task_code/task_compare_paydollar_sf.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import warnings
+import logging
 
 """
 To create code for 2 part process.
@@ -9,7 +10,7 @@ To create code for 2 part process.
 
 """
 
-def main():
+def task_compare_paydollarsf(folder_path):
     # Ignore warnings for stylesheets
     warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl.styles.stylesheet')
     # get folder path
@@ -58,13 +59,13 @@ def main():
     filtered_merge_df = merge_df[condition]
 
     if filtered_merge_df.empty:
-        print('No data need to be changed to Close Won!')
+        logging.info('No data need to be changed to Close Won!')
     else:
         # compare
         condition2 = paydollar_column.isin(filtered_merge_df['External Reference Id'])
         to_set_closewon_list = paydollar_column[condition2].dropna()
 
-        print('Creating file with data to change stage to Close Won!')
+        logging.info('Creating file with data to change stage to Close Won!')
         to_set_closewon_list.to_excel(os.path.join(folder_path, 'to_set_closewon.xlsx'), index=False)
 
     # compare the merge column and paydollar column 
@@ -74,10 +75,10 @@ def main():
     # not in merge meaning there are transaction not created
     # so if the df is empty the print status else create file
     if not_in_merge_df.empty:
-        print('All transactions have been created!')
+        logging.info('All transactions have been created!')
     else:
         not_in_merge_df.to_excel(os.path.join(folder_path, 'transaction_not_created.xlsx'), index=False)
-        print('A list of transaction not created has been created!')
+        logging.info('A list of transaction not created has been created!')
 
 
     
@@ -97,6 +98,3 @@ def main():
 
 
     
-
-if __name__ == '__main__':
-    main()
