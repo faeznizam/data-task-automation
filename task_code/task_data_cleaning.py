d17ed6f6e10ca4e8f33308f1ee80af7c3e763f7f
Muhammad Faez Mohammad Nizam <faeznizam@gmail.com>
2024-06-10 16:11:16 +0800

    update file

--- task_code/task_data_cleaning.py
+++ task_code/task_data_cleaning.py
@@ -1,178 +1,76 @@
 # import from sub files
-from dependencies import clean_gender
-from dependencies import clean_icnumber
-from dependencies import clean_birthdate
-from dependencies import clean_email
-from dependencies import clean_ethnic
-from dependencies import clean_phone_number
-from dependencies import clean_file_name
+
+from .dependencies import (
+    clean_gender,clean_icnumber, clean_birthdate, clean_email, clean_ethnic, 
+    clean_phone_number, clean_file_name, date_handler
+    )
 
 # import from module
 import pandas as pd
-import os
 import warnings
-from datetime import datetime
 import logging
-
+import os
 
 def create_file_path(folder_path, file):
+    # create file path
     return os.path.join(folder_path, file)
 
-def create_date_format():
-    current_date = datetime.now()
-    date_format = current_date.strftime('%Y-%m-%d')
-    return date_format
-
 def rename_file(file, date_format):
-    extract_filename = file[:-5]
-    new_file_name = f'{extract_filename}_{date_format}.xlsx'
-    return new_file_name
+    # rename file by getting original file and add date
+    original_filename = file[:-5]
+    return f'{original_filename}_{date_format}.xlsx'
 
-def save_file(folder_path, new_file_name, df):
-    file_path = os.path.join(folder_path, new_file_name)
-    df.to_excel(file_path, index=False)
-    print(f'{new_file_name} has been saved in the folder!')
+def process_file(file, folder_path, date_format, clean_function):
+    file_path = create_file_path(folder_path, file)
+    df = pd.read_excel(file_path)
 
-def convert_date_format(df):
-    df['Created Date'] = pd.to_datetime(df['Created Date'], format='%d/%m/%Y')
-    df['Created Date'] = df['Created Date'].dt.strftime('%Y-%m-%d')
+    # check if national id column exist
+    if 'National ID' in df.columns:
+        df['National ID'] = df['National ID'].astype(str)
 
-    return df
+    # process function
+    df = clean_function(df)
 
+    df['Date'] = date_format
+
+    # change date format in one of the column
+    df = date_handler.convert_date_format(df)
+    new_file_name = rename_file(file, date_format)
+    new_file_path = create_file_path(folder_path, new_file_name)
+    df.to_excel(new_file_path, index=False)
+
+    logging.info(f'{new_file_name} has been created. ')
+    
 def task_data_cleaning_main(folder_path):
 
-    logging.info("Start data cleaning process...")
+    #folder_path = r'C:\Users\mfmohammad\OneDrive - UNICEF\Documents\Codes\PortableApp\task_code\test_data\task_data_cleaning'
 
     # ignore warning
     warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl.styles.stylesheet')
-    # clean timestamp in file name
-    clean_file_name.clean_name(folder_path)
-    # create data formatting
-    date_format = create_date_format()
+    # remove timestamp in file name
+    clean_file_name.remove_timestamp(folder_path)
 
-    # iterate the folder
+    # get current date in YYYY-MM-DD format
+    date_format = date_handler.current_date_format()
+
+    file_cleaning_map = {
+        'Donor With Invalid Email.xlsx' : clean_email.clean_email_file, 
+        'Donor With Invalid IC.xlsx' : clean_icnumber.clean_ic_file,
+        'Donor With Invalid Phone Number.xlsx' : clean_phone_number.clean_phone_file, 
+        'Donor Without Age and Birthdate.xlsx' : clean_birthdate.clean_birthdate_file,
+        'Donor Without Ethnic.xlsx' : clean_ethnic.clean_ethnic_file,
+        'Donor Without Gender.xlsx' : clean_gender.clean_gender_file 
+
+    }
+
     for file in os.listdir(folder_path):
-
-        if 'Donor With Invalid Email.xlsx' in file:
-            # build file path
-            file_path = create_file_path(folder_path, file)
-            # read the file
-            df = pd.read_excel(file_path)
-
-            # process the file
-            df = clean_email.rename_column(df)
-            df['Email'] = df['Original Email'].apply(clean_email.check_email)
-            df['npe01__HomeEmail__c'] = df['Original Email'].apply(clean_email.check_email)
-            df['npe01__Preferred_Email__c'] = df['Original Email'].apply(clean_email.check_email)
-            df['npe01__WorkEmail__c'] = df['Original Email'].apply(clean_email.check_email)
-            df['Date'] = create_date_format()
-
-            # rename and save the file
-            date_format = create_date_format()
-            df = convert_date_format(df)
-            new_file_name = rename_file(file, date_format)
-            save_file(folder_path, new_file_name, df)
-
-            logging.info('Email file has been cleaned.')
-
-        elif 'Donor With Invalid IC.xlsx' in file:
-            # building file path
-            file_path = create_file_path(folder_path, file)
-            # read the file
-            df = pd.read_excel(file_path, dtype = {'National ID': str})
-
-            # process the file
-            df['Updated National ID'] = df['National ID'].apply(clean_icnumber.validate_nat_id)
-            df = clean_icnumber.blank_invalid_ic(df)
-            df = clean_icnumber.rename_column(df)
-            df['Date'] = create_date_format()
-
-            # rename and save the file
-            date_format = create_date_format()
-            df = convert_date_format(df)
-            new_file_name = rename_file(file, date_format)
-            save_file(folder_path, new_file_name, df)
-
-            logging.info('IC file has been cleaned.')
-
-        elif 'Donor With Invalid Phone Number.xlsx' in file:
-            # build file path and read the file
-            file_path = create_file_path(folder_path, file)
-            df = pd.read_excel(file_path)
-
-            # clean the data
-            df = clean_phone_number.process_mobile_numbers(df)
-            df = clean_phone_number.rename_column(df)
-            df['Date'] = create_date_format()
-
-            # rename and save the data
-            date_format = create_date_format()
-            df = convert_date_format(df)
-            new_file_name = rename_file(file, date_format)
-            save_file(folder_path, new_file_name, df)
-
-            logging.info('Phone Number file has been cleaned.')
-
-        elif 'Donor Without Age and Birthdate.xlsx' in file:
-            # build file path and read the file
-            file_path = create_file_path(folder_path, file)
-            df = pd.read_excel(file_path, dtype = {'National ID': str})
-
-            # clean the data
-            df['National ID'] = df['National ID'].apply(clean_birthdate.validate_nat_id)
-            df['Birthdate'] = df['National ID'].apply(clean_birthdate.calculate_birthdate)
-            df['Age'] = df['Birthdate'].apply(clean_birthdate.calculate_age)
-            df['Date'] = create_date_format()
-
-            # rename and save the file
-            date_format = create_date_format()
-            df = convert_date_format(df)
-            new_file_name = rename_file(file, date_format)
-            save_file(folder_path, new_file_name, df)
-
-            logging.info('Birthdate file has been cleaned.')
-            
-        elif 'Donor Without Ethnic.xlsx' in file:
-            # build file path and read the file
-            file_path = create_file_path(folder_path, file)
-            df = pd.read_excel(file_path)
-
-            # clean the data
-            df['Ethnic'] = df['Full Name'].apply(clean_ethnic.categorize_ethnic)
-            df = clean_ethnic.rename_column(df)
-            df['Date'] = create_date_format()
-
-            # rename and save the file
-            date_format = create_date_format()
-            df = convert_date_format(df)
-            new_file_name = rename_file(file, date_format)
-            save_file(folder_path, new_file_name, df)
-
-            logging.info('Ethnic file has been cleaned.')
-
-        elif 'Donor Without Gender.xlsx' in file:
-            # build file path and rename file
-            file_path = create_file_path(folder_path, file)
-            df = pd.read_excel(file_path, dtype = {'National ID': str})
-
-            # clean the data
-            df['National ID'] = df['National ID'].apply(clean_icnumber.validate_nat_id)
-            df = clean_gender.gender_by_national_id(df)
-            df = clean_gender.rename_column(df)
-            df['Date'] = create_date_format()
-            
-            # rename and save the data
-            date_format = create_date_format()
-            df = convert_date_format(df)
-            new_file_name = rename_file(file, date_format)
-            save_file(folder_path, new_file_name, df)
-
-        else:
-            pass
+        if file in file_cleaning_map:
+            process_file(file, folder_path, date_format, file_cleaning_map[file])
+        
 
 
+"""
+if __name__ == '__main__':
+    task_data_cleaning_main()
 
-
-
-
-
+"""
