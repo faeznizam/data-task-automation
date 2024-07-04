43283e22f28ef38a408abc570fd4c77f9afaeb7a
Muhammad Faez Mohammad Nizam <faeznizam@gmail.com>
2024-07-04 12:31:47 +0800

    update code

--- task_code/onhold.py
+++ task_code/onhold.py
@@ -1,5 +1,5 @@
 # import module from folder
-from .dependencies import process_onhold_startek, process_onhold_uts, process_onhold_ds
+from .dependencies import process_onhold_uts, process_onhold_ds
 from .dependencies import helper_analyze, helper_deletion
 
 # import dependencies
@@ -16,35 +16,6 @@ def onhold_flow(folder_path):
    # initiate list
    processed_file_info = []
    deleted_list = []
-   deleted_list_startek = []
-
-   # startek process
-   # build path for agency folder
-   agency_folder = os.path.join(folder_path, 'Startek')
-   # loop for subfolder in agency folder
-   for sub_folder in os.listdir(agency_folder):
-      # if subfolder has HR and SR build subfolderpath
-      if sub_folder in ['HR', 'SR']:
-         sub_folder_path = os.path.join(agency_folder, sub_folder)
-         # inside each subfolder, get file list. 
-         file_list = os.listdir(sub_folder_path)
-         # check for keyword in all file name
-         for file in file_list:
-            if 'New Onhold' in file:
-               
-               # process files
-               original_df, updated_df, excluded_df, new_file_name = process_onhold_startek.process_startek(sub_folder_path, file)
-
-               # get deleted rows
-               helper_deletion.get_deleted_info(excluded_df, deleted_list_startek, new_file_name)
-            
-               # get row count
-               helper_analyze.get_row_count(original_df, updated_df, new_file_name, processed_file_info)
-               
-   # create deleted list
-   helper_deletion.create_deleted_list_startek(deleted_list_startek, folder_path)
-
-   logging.info('Startek process completed.')
    
    # process UTS 
    # build folder path for agency folder
