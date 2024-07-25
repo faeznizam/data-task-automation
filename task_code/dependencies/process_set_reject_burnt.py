9c992d00afa9ac69cc5b8780e1f5a8e5ec0d478b
Muhammad Faez Mohammad Nizam <faeznizam@gmail.com>
2024-07-25 15:09:51 +0800

    update code

--- task_code/dependencies/process_set_reject_burnt.py
+++ task_code/dependencies/process_set_reject_burnt.py
@@ -8,12 +8,9 @@ def process_data(file_path, folder_path, file, date, status, closed_reason, colu
 
     # rename file based on process
     if status == 'Burnt':
-        df = df.rename(columns={'Id': 'sescore__Pledge_Id__c'})
+        df = df.rename(columns={'Pledge ID': 'Id'})
     elif status == 'Rejected':
-        row_to_check = 1
-        for column in df.columns:
-            if df[column].astype(str).iloc[row_to_check].startswith('PD'):
-                df = df.rename(columns={column: 'sescore__Pledge_Id__c' })
+        df = df.rename(columns={'Pledge ID': 'Id'})
 
     # populate column with date, status, and closed reason accordingly. 
     df['npsp__EndDate__c'] = date
@@ -25,13 +22,13 @@ def process_data(file_path, folder_path, file, date, status, closed_reason, colu
 
     # save file to excel and csv
     df.to_excel(os.path.join(folder_path, f'{file[:-25]}_to_upload.xlsx'), index=False)
-    df.to_csv(os.path.join(folder_path, f'{file[:-25]}_to_upload.csv'), index=False, encoding='utf-8')
+    #df.to_csv(os.path.join(folder_path, f'{file[:-25]}_to_upload.csv'), index=False, encoding='utf-8')
 
 # function for burnt file
 def process_burnt(file_path, folder_path, file, date):
     # get column to delete
     column_to_drop_burnt = [
-        'Supporter ID', 'Pledge ID', 'First Name', 'Last Name', 'Signup Date', 'First Donation Date', 'End Date', 
+        'Supporter ID', 'First Name', 'Last Name', 'Signup Date', 'First Donation Date', 'End Date', 
         'Campaign: Campaign Name', 'Pledge Frequency', 'Status', 'Stage'
     ]
 
@@ -42,7 +39,7 @@ def process_burnt(file_path, folder_path, file, date):
 def process_reject(file_path, folder_path, file, date):
     # get column to delete
     column_to_drop_reject = [
-        'Supporter ID', 'Pledge ID.1', 'First Name', 'Last Name', 'Signup Date', 'First Donation Date', 'Last Donation Date', 
+        'Supporter ID', 'First Name', 'Last Name', 'Signup Date', 'First Donation Date', 'Last Donation Date', 
         'End Date', 'Campaign: Campaign Name', 'Status', 'Stage', 'Pledge Frequency'
     ]
 
