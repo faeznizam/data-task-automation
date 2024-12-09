815e2683b707c0874dfa00a09871ba7e4401cf53
Muhammad Faez Mohammad Nizam <faeznizam@gmail.com>
2024-12-09 16:07:43 +0800

    Update code

--- task_code/dependencies/process_onetime_conversion.py
+++ task_code/dependencies/process_onetime_conversion.py
@@ -1,51 +1,56 @@
-# import from local module
+# local dependencies for the file
 from .helper_mobile_phone import process_mobile_numbers, delete_condition
-from .helper_duplication import remove_duplicates
 
 # import library
-from datetime import datetime
-import pandas as pd
-import os
+from datetime import datetime # to handle data time related 
+import pandas as pd # to manipulate data 
+import os # to interact with file system
 
-def count_by_campaign(updated_df):
+def calculate_count_by_campaign(updated_df):
     campaign_count = updated_df.groupby('Campaign Name').size()
-    result = campaign_count.reset_index(name='Counts')
-    return result
+    return campaign_count.reset_index(name='Counts')
 
-def rename_file():
+def create_file_name():
     current_date = datetime.now()
     date_format = current_date.strftime('%Y%m%d')
-    new_file_name = f'TMOC_UTS_{str(date_format)}.xlsx'
-    return new_file_name
+    return f'TMOC_UTS_{str(date_format)}.xlsx'
 
 def process_file(folder_path, file_name):
-    # get path
-    file_path = os.path.join(folder_path, file_name)
+    """
+    To process one time conversion file
 
-    # read file and make postcode column a string
+    Parameter:
+    - folder path (str): file directory contain file to process.
+    - file name (str) name of file to process. 
+
+    Output:
+    - original_df: original dataframe
+    - updated_df: the updated dataframe
+    - excluded_df: dataframe contain excluded rows
+    - new_file_name: name of the newly created file
+
+    """
+
+    # create file path and read the file
+    file_path = os.path.join(folder_path, file_name)
     original_df = pd.read_excel(file_path, dtype={'Post Code': str})
     
-    # assign df to new variable
-    updated_df = original_df
-    
-    # clean phone number in column
-    updated_df = process_mobile_numbers(updated_df, 'Mobile Phone')
-
-    # exclude invalid number rows and assign to new dataframe
+    # process phone number and apply delete condition to data
+    updated_df = process_mobile_numbers(original_df, 'Mobile Phone')
     rows_to_exclude = delete_condition(updated_df, 'Mobile Phone')
+
+    # use boolean indexing to exclude and update dataframe
     excluded_df = updated_df[rows_to_exclude]
+    updated_df = updated_df[~rows_to_exclude]
 
-    # use opposite condition to filter the wanted number
-    rows_to_update = ~ rows_to_exclude
-    updated_df = updated_df[rows_to_update]
+    # remove duplicates
+    updated_df.drop_duplicates(subset='Mobile Phone', inplace=True)
 
-    # remove duplicate based on column
-    updated_df = remove_duplicates(updated_df, 'Mobile Phone')
-
-    # rename file using function, build new file path and save file
-    new_file_name = rename_file()
+    # create file name, file path and save file to excel
+    new_file_name = create_file_name()
     new_file_path = os.path.join(folder_path, new_file_name)
     updated_df.to_excel(new_file_path, index=False)
+
     return original_df, updated_df, excluded_df, new_file_name
 
 
